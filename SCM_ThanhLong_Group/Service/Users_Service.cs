9da45ca65e16b4983de5dae2f2b1b1b60f56a3d7
Cao Hoàng Khánh Băng <caohoangkhanhbang2003@gmail.com>
2024-11-27 20:44:23 +0700

    Đổi giao diện nhưng còn lỗi

--- SCM_ThanhLong_Group/Service/Users_Service.cs
+++ SCM_ThanhLong_Group/Service/Users_Service.cs
@@ -28,13 +28,11 @@ namespace SCM_ThanhLong_Group.Service
             //string connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl1;";
             if (string.Compare(username, "sys", true) == 0 || string.Compare(username, "system", true) == 0)
             {
-                //connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/chkb;";
-                connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/orcl;";
+                connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/orcl1;";
             }
             else
             {
-                //connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/chkb;";
-                connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl;pooling=false";
+                connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl1;pooling=false";
             }
             if(string.Compare(username,"sys", true)==0)
             {
