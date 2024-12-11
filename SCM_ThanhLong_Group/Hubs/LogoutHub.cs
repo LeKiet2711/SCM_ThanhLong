f6bd5a6afcbd559fb8e613d20e757e5e65a3a5fc
Cao Hoàng Khánh Băng <caohoangkhanhbang2003@gmail.com>
2024-12-11 21:08:27 +0700

    THÊM THAY ĐỔI CHUỖI KẾT NỐI

---
+++ SCM_ThanhLong_Group/Hubs/LogoutHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SCM_ThanhLong_Group
+{
+    public class LogoutHub : Hub
+    {
+        public async Task BroadcastLogout(string username)
+        {
+            await Clients.Group(username).SendAsync("Logout");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var username = Context.User.Identity.Name;
+            await Groups.AddToGroupAsync(Context.ConnectionId, username);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var username = Context.User.Identity.Name;
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, username);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
