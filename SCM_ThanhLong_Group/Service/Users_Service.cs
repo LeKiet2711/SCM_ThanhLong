75689a8187e5f87e8fdf138b2e2bd3ae2b12776e
Cao Hoàng Khánh Băng <caohoangkhanhbang2003@gmail.com>
2024-12-13 01:07:18 +0700

    Thêm chuỗi kết nối

--- SCM_ThanhLong_Group/Service/Users_Service.cs
+++ SCM_ThanhLong_Group/Service/Users_Service.cs
@@ -28,13 +28,15 @@ namespace SCM_ThanhLong_Group.Service
             //string connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl1;";
             if (string.Compare(username, "sys", true) == 0 || string.Compare(username, "system", true) == 0)
             {
-                connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/orcl1;";
-                //connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/orcl;";
+                //connectionString = $"User Id={username};Password={password};Data Source=26.249.114.107:1521/orcl1;";
+                //connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/orcl1;";
+                connectionString = $"User Id={username};Password={password};Data Source=localhost:1521/orcl;";
             }
             else
             {
-                connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl1;pooling=false";
-                //connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl;pooling=false";
+                //connectionString = $"User Id=C##{username};Password={password};Data Source=26.249.114.107:1521/orcl1;pooling=false";
+                //connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl1;pooling=false";
+                connectionString = $"User Id=C##{username};Password={password};Data Source=localhost:1521/orcl;pooling=false";
             }
             if(string.Compare(username,"sys", true)==0)
             {
