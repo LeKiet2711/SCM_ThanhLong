f7987889005fb40d50d4aa7d066b5d7fa335955f
LeKiet2711 <legiakiet03@gmail.com>
2025-02-20 11:59:17 +0700

    Update follow repository

--- SCM_ThanhLong_Group/Program.cs
+++ SCM_ThanhLong_Group/Program.cs
@@ -10,6 +10,8 @@ using SCM_ThanhLong_Group.Service;
 using Telerik.Reporting.Services;
 using Telerik.Reporting.Cache.File;
 using BlazorDownloadFile;
+using SCM_ThanhLong_Group.Interface;
+using SCM_ThanhLong_Group.Repository;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +30,8 @@ builder.Services.TryAddSingleton<IReportServiceConfiguration>(sp => new ReportSe
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<OracleFgaService>();
+
+builder.Services.AddScoped<ICheckEmail, CheckEmailRepository>();
 builder.Services.AddScoped<checkEmail>();
 builder.Services.AddBlazoredToast();
 builder.Services.AddBlazorDownloadFile();
@@ -35,26 +39,56 @@ builder.Services.AddScoped<OracleAuditService>();
 builder.Services.AddSingleton<ConnectionStringManager>();
 builder.Services.AddScoped<OracleDbConnection>();
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IKeyValidationRepository, KeyValidationRepository>();
 builder.Services.AddScoped<KeyValidationService>();
 builder.Services.AddScoped<VerificationService>();
+
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 builder.Services.AddScoped<Users_Service>();
 builder.Services.AddScoped<Users>();
+
+builder.Services.AddScoped<IChucNangRepository, ChucNangRepository>();
 builder.Services.AddScoped<ChucNang_Service>();
+
+builder.Services.AddScoped<IKhuVucTrongRepository, KVTRepository>();
 builder.Services.AddScoped<KhuVucTrong_Service>();
+
+builder.Services.AddScoped<IHoTrongRepository, HoTrongRepository>();
 builder.Services.AddScoped<HoTrong_Service>();
+
+builder.Services.AddScoped<IKhoRepository, KhoRepository>();
 builder.Services.AddScoped<Kho_Service>();
+
+builder.Services.AddScoped<IKhoUserRepository, KhoUserRepository>();
 builder.Services.AddScoped<KhoUser_Service>();
+
+builder.Services.AddScoped<IPhieuNhapRepository, PhieuNhapRepository>();
 builder.Services.AddScoped<PhieuNhap_Service>();
+
+builder.Services.AddScoped<IPhieuXuatRepository, PhieuXuatRepository>();
 builder.Services.AddScoped<PhieuXuat_Service>();
+
+builder.Services.AddScoped<ICTPNRepository, CTPNRepository>();
 builder.Services.AddScoped<ChiTietPhieuNhap_Service>();
+
+builder.Services.AddScoped<ICTPXRepository, CTPXRepository>();
 builder.Services.AddScoped<ChiTietPhieuXuat_Service>();
+
+builder.Services.AddScoped<ITonKhoRepository, TonKhoRepository>();
 builder.Services.AddScoped<TonKho_Service>();
 builder.Services.AddScoped<Profile_Service>();
+
+builder.Services.AddScoped<ILoThanhLongRepository, LoThanhLongRepository>();
 builder.Services.AddScoped<LoThanhLong_Service>();
+
+builder.Services.AddScoped<IAuditRepository, AuditRepository>();
 builder.Services.AddScoped<Audit_Service>();
+
+builder.Services.AddScoped<IDBA_UserRepository, DBA_UserRepository>();
 builder.Services.AddScoped<DBA_UserService>();
 
-
+builder.Services.AddScoped<INhomQuyenRepository, NhomQuyenRepository>();
 builder.Services.AddScoped<NhomQuyen_Service>();
 
 builder.Services.AddBlazoredSessionStorage();
